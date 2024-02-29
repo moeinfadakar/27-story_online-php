ce72fda70b51051262168470248c643d0d01a654
moeinfadakar <109180018+moeinfadakar@users.noreply.github.com>
2024-02-29 18:06:47 +0330

    Add files via upload

---
+++ tell_me_story/php/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+<link rel="stylesheet" href="style.css" type="text/css" >
+<link rel="stylesheet" href="" type="text/css" >
+
+
+
+
+</head>
+<body>
+    
+<div class="container">
+<div class="space">
+
+<!-- here is the space that you can see the story :  -->
+
+<h3> </h3>
+
+<form action="answers.php" method="POST">
+
+<!-- choice that you have :  -->
+
+<button type="button" name="first_answer" class="btn btn-secondary btn-lg"> </button>
+
+<button type="button" name="second_answer" class="btn btn-secondary btn-lg"></button>
+
+</form>
+</div>
+</div>
+
+
+
+</body>
+</html>
